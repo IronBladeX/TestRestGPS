8d5db304abddff820f557d1436a2e3fe9aebba4e
IronBladeX <xironbladex@gmail.com>
2017-09-09 10:51:38 +0200

    Add Dependencies
    Add .jsonpath
    Add mongodb
    
    Add Comment On MainApplication

--- src/main/java/com/testrest/gps/GpsApplication.java
+++ src/main/java/com/testrest/gps/GpsApplication.java
@@ -3,10 +3,13 @@ package com.testrest.gps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * Main Application
+ * @author Alexandre PERROT
+ */
 @SpringBootApplication
 public class GpsApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(GpsApplication.class, args);
-	}
+    public static void main(String[] args) {
+            SpringApplication.run(GpsApplication.class, args);
+    }
 }

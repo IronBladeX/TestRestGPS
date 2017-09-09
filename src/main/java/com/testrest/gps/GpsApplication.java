b39025dd0af4c1366520cf2928257956000c47f4
IronBladeX <xironbladex@gmail.com>
2017-09-09 10:44:26 +0200

    Initial Commit Project
    Add Structure Files of Project

---
+++ src/main/java/com/testrest/gps/GpsApplication.java
@@ -0,0 +1,12 @@
+package com.testrest.gps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GpsApplication.class, args);
+	}
+}
